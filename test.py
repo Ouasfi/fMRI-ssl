49ab04d8862af6a5a0a2a2cce8f1949fb3e51563
Ouasfi <amine1ouasfi@hotmail.f>
2021-03-20 18:05:08 +0100

    add mutli_sbjects tests

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 
+# %%
 import RP_dataset as rpd
 import model as md
 import torch
 from torch.nn.functional import soft_margin_loss
-
 
 if __name__ == "__main__":
     # %%
@@ -17,6 +17,18 @@ if __name__ == "__main__":
     for i, data in enumerate(val_loader):
         assert abs(data[0][0] - val_list[i][0][0]).sum() == 0
     print('\nLoaders sucessfully initialised \n')
+    # %%
+    val_generator = rpd.RP_Dataset_Multi( subjects = ['048', '096', '101'], sampling_params= (1,90), wind_len = 15 ,mode = "val")
+    val_sampler = rpd.RPSampler(val_generator, batch_size = 30,size = 60,  weights = [0.5]*2)
+    val_dataset = torch.utils.data.Subset(val_generator, indices= list(val_sampler))
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=30, 
+                                         num_workers=0, collate_fn=rpd.ssl_collate)
+    # %%
+    val_list = list(val_loader)
+    for i, data in enumerate(val_loader):
+        assert abs(data[0][0] - val_list[i][0][0]).sum() == 0
+    print('\nLoaders sucessfully initialised \n')
+
     # %%
     print('- Audio encoder ...\n')
     m = md.AudioEmbed()
