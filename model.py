1fe6b36f8cabe5a388fd47ed991a64d2c711903e
aouasfi <amine.ouasfi@inria.fr>
2021-04-13 03:20:17 +0200

    add cross_corr model and loss_fn

--- model.py
+++ model.py
@@ -79,12 +79,12 @@ class SiameseModel(nn.Module):
         x_fmri = self.flatten( self.fmri(x_fmri.float()) )
         x = self.metric(torch.abs(x_audio - x_fmri))
         return x
-class SiameseModel_2(nn.Module):
+class SiameseModel_cat(nn.Module):
     """
     SiameseModel
     """
     def __init__(self, hidden_dim, n_classes = 2, voxels = 3):
-        super(SiameseModel_2, self).__init__()
+        super(SiameseModel_cat, self).__init__()
         self.audio = AudioEmbed().float()
         self.fmri = FMRIEmbed(voxels).float()
         self.metric = nn.Linear(hidden_dim*2, out_features = n_classes)
@@ -95,7 +95,6 @@ class SiameseModel_2(nn.Module):
         x_fmri = self.flatten( self.fmri(x_fmri.float()) )
         x = self.metric(torch.cat([x_audio , x_fmri], dim = 1))
         return x
-
 class ToDevice(Callback):
     "Move data to CUDA device and convert it to float"
     def __init__(self, device=None): self.device = torch.device('cuda')#ifnone(device, DEVICE)
@@ -103,3 +102,47 @@ class ToDevice(Callback):
         self.learn.xb = (self.learn.xb[0][0].float(), self.learn.xb[0][1].float())
         self.learn.xb,self.learn.yb = to_device(self.xb),to_device(self.yb)
     def before_fit(self): self.model.to(self.device)
+
+def off_diagonal(x):
+    # return a flattened view of the off-diagonal elements of a square matrix
+    n, m = x.shape
+    assert n == m
+    return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+class SiameseModel_corr(nn.Module):
+    """
+    SiameseModel
+    """
+
+    def __init__(self, hidden_dim, n_classes = 2, voxels = 3):
+        super(SiameseModel_corr, self).__init__()
+        self.audio = AudioEmbed().float()
+        self.fmri = FMRIEmbed(voxels).float()
+        #self.metric = md.Cross_corr(hidden_dim = 32*10)
+        self.flatten= nn.Flatten()
+    def forward(self, x_fmri, x_audio):
+        #x_fmri, x_audio = fmri_audio
+        x_audio = self.flatten(self.audio(x_audio))
+        x_fmri = self.flatten( self.fmri(x_fmri.float()) )
+        #x = self.metric(x_audio , x_fmri)
+        return (x_fmri, x_audio)
+
+class CrosscCorrLoss(nn.Module):
+    """
+    Cross_corr
+    """
+    def __init__(self, hidden_dim, scale = 1, lambd = 0.001):
+        super(CrosscCorrLoss, self).__init__()
+        self.bn = nn.BatchNorm1d(hidden_dim, affine=False)
+        self.bn2 = nn.BatchNorm1d(hidden_dim, affine=False)
+        self.scale_loss = scale
+        self.lambd = lambd
+    def forward(self, out, y ):
+        y = 2*y.float()-1
+        x_fmri, x_audio = out[0], out[1]
+        N, D = x_fmri.shape
+        c = (self.bn(x_fmri).mul_(y)).T@(self.bn2(x_audio))
+        c.div_(N)
+        on_diag = torch.diagonal(c).add_(-1).pow_(2).sum().mul(self.scale_loss)
+        off_diag = off_diagonal(c).pow_(2).sum().mul(self.scale_loss)
+        return on_diag + self.lambd * off_diag
